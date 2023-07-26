3f41fde3c65bff616cbbb75786ceb2fce81f3f81
Shuwen Wang <sherwin.water@gmail.com>
2023-07-26 10:56:06 -0400

    fix styling

--- src/context/MantineProvider.tsx
+++ src/context/MantineProvider.tsx
@@ -1,21 +1,36 @@
 "use client";
 
-import { MantineProvider as DefaultMantineProvider } from "@mantine/core";
+import {
+  MantineProvider as DefaultMantineProvider,
+  ColorSchemeProvider,
+  ColorScheme,
+} from "@mantine/core";
+import { useColorScheme } from "@mantine/hooks";
+import { useState } from "react";
 
 export default function MantineProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const preferredColorScheme = useColorScheme();
+  const [colorScheme, setColorScheme] =
+    useState<ColorScheme>(preferredColorScheme);
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
   return (
-    <DefaultMantineProvider
-      withGlobalStyles
-      withNormalizeCSS
-      theme={{
-        colorScheme: "light",
-      }}
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
     >
-      {children}
-    </DefaultMantineProvider>
+      <DefaultMantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{ colorScheme }}
+      >
+        {children}
+      </DefaultMantineProvider>
+    </ColorSchemeProvider>
   );
 }
--- src/context/QueryProvider.tsx
+++ src/context/QueryProvider.tsx
@@ -1,13 +1,20 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental'
+
 import React, { useState } from "react";
 
 const QueryProvider = ({ children }: { children: React.ReactNode }) => {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      
+      <ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
+
+      
+      </QueryClientProvider>
   );
 };
 
